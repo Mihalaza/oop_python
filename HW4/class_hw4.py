00a984d1d098b6a2cbbb12f499cae907a4cf0bde
MihaDell <miha.lazarchuk@gmail.com>
2023-05-20 18:16:59 +0200

    HOMEWORK3

---
+++ HW4/class_hw4.py
@@ -0,0 +1,36 @@
+class BaseClass1:
+    def __init__(self):
+        self.base_attr1 = "Base Attribute 1"
+
+    def base_method1(self):
+        print("Base Method 1")
+
+class BaseClass2:
+    def __init__(self):
+        self.base_attr2 = "Base Attribute 2"
+
+    def base_method2(self):
+        print("Base Method 2")
+
+class DerivedClass(BaseClass1, BaseClass2):
+    def __init__(self):
+        BaseClass1.__init__(self)
+        BaseClass2.__init__(self)
+        self.derived_attr = "Derived Attribute"
+
+    def derived_method(self):
+        print("Derived Method")
+
+
+derived_obj = DerivedClass()
+
+
+print(derived_obj.base_attr1)
+derived_obj.base_method1()
+
+print(derived_obj.base_attr2)
+derived_obj.base_method2()
+
+
+print(derived_obj.derived_attr)
+derived_obj.derived_method()

e413be3cfe0f9ccc2fc743d2b73da6e865913659
MihaDell <miha.lazarchuk@gmail.com>
2023-05-20 18:05:45 +0200

    HOMEWORK3

--- Lesson3/human.py
+++ Lesson3/human.py
@@ -3,16 +3,6 @@ class Human:
    def __init__(self, name, role):
      self.Name = name
      self.Role = role
-        '''
-        t or f = t
-        f or t = t
-        t or t = t
-        f or f = f
-        t and t = t
-        t and f = f
-        f and t = f
-        f and f = f
-        '''
-  def ToString(self):
+   def ToString(self):
     print(f"Name: {self.Name}\n"
     f"Role: {self.Role}")
